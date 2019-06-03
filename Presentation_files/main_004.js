085f444d7a89c794df8eab90e79fe538e5b68a35
Zordo <pedrogodinhoc@gmail.com>
2019-06-03 15:09:46 +0200

    2ndcommit

--- Presentation_files/main_004.js
+++ Presentation_files/main_004.js
@@ -1,35 +1,29 @@
-// Small extension to add an help menu pointing 
-// to jupyter_contrib_nbextensions at readthedocs.
+// nbextension by jcb91
+// Tiny nbextension to add an edit-menu item to open the NbExtensions config page
 
-define(['jquery', 'base/js/namespace'], function($, Jupyter) {
+define(['jquery', 'base/js/namespace'], function ($, Jupyter) {
     "use strict";
 
-    function add_help_menu_item() {
+    var load_ipython_extension = function () {
+        var menu_item = $('<li/>').append(
+            $('<a/>', {
+                'target' : '_blank',
+                'title' : 'Opens in a new window',
+                'href' : Jupyter.notebook.base_url + 'nbextensions/',
+            })
+            .append(' ')
+            .append($('<i/>', {'class' : 'fa fa-cogs menu-icon pull-right'}))
+            .append($('<span/>').html('nbextensions config'))
+        );
 
-        if ($('#jupyter_contrib_nbextensions_help').length > 0) {
-            return;
-        }
-        var menu_item = $('<li/>')
-            .append(
-                $('<a/>')
-                .html('Jupyter-contrib <br> nbextensions')
-                .attr('title', 'Jupyter_contrib_nbextensions documentation')
-                .attr('id', "jupyter_contrib_nbextensions_help")
-                .attr('href', 'http://jupyter-contrib-nbextensions.readthedocs.io/en/latest/')
-                .attr('target', "_blank")
-                .append(
-                    $('<i/>')
-                    .addClass('fa fa-external-link menu-icon pull-right')
-                ))
-        menu_item.insertBefore($($("#help_menu > .divider")[1]))
-    }
-
-
-    var load_ipython_extension = function() {
-        add_help_menu_item();
+        var edit_menu = $('#edit_menu');
+        edit_menu.append($('<li/>').addClass('divider'));
+        edit_menu.append(menu_item);
     };
 
-    return {
-        load_ipython_extension: load_ipython_extension
+    // export the extension so it can be loaded correctly
+    var extension = {
+        load_ipython_extension : load_ipython_extension
     };
+    return extension;
 });
